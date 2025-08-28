536a9fc5124060671af1fa82d79d987c16c535e3
EvgenyStishenok99 <stishenok999@mail.ru>
2025-08-28 14:34:38 +0300

    comlite ferst dz

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,15 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
+const num1 = 40
+const num2 = 45
 
-function calculateAverage() {}
+function calculateAverage(first_number, second_number) {
+ const sum = first_number + second_number;
+ const average = sum/2
+return average;
+ }
+
+
+const fullNum = calculateAverage(num1,num2)
+
+console.log(fullNum)
+
