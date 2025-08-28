1c614c651ccc6e3a1fdb08ba3e33149711430bbf
EvgenyStishenok99 <stishenok999@mail.ru>
2025-08-28 17:34:39 +0300

    complite second dz

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,16 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
+const myNumber = 112
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true
 
-function isEven() {}
+  }
+  else  {
+    return false
+
+  }
+  }
+
+  const result = isEven(myNumber)
+console.log(result)
