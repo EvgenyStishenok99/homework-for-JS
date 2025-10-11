b2793cbbe2128f66d9e4a1bea41a992c2fa3b55c
EvgenyStishenok99 <stishenok999@mail.ru>
2025-10-11 12:57:48 +0300

    dom complite

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -17,3 +17,45 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+const web_tech_image = document.getElementById('web-tech-image')
+const prev_button = document.getElementById('prev-button')
+const next_button = document.getElementById('next-button')
+
+
+let currentImageIndex = 0;
+
+function updateImage() {
+  web_tech_image.src = WEB_TECH_IMAGES[currentImageIndex];
+}
+
+
+function nextImage() {
+  currentImageIndex = (currentImageIndex + 1) % WEB_TECH_IMAGES.length;
+  updateImage();
+}
+
+// Функция для перехода к предыдущему изображению
+function prevImage() {
+  currentImageIndex = (currentImageIndex - 1 + WEB_TECH_IMAGES.length) % WEB_TECH_IMAGES.length;
+  updateImage();
+}
+
+prev_button.addEventListener('click', prevImage);
+next_button.addEventListener('click', nextImage);
+
+
+updateImage();
+
+
+
+
+
+
+
+
+
+
+
+
+
